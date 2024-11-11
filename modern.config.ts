592b636b078d513b4e320ee8eed4e46721584501
zhaoyi <291420120@qq.com>
2024-11-11 11:22:55 +0800

    update modern settings

--- modern.config.ts
+++ modern.config.ts
@@ -1,7 +1,18 @@
 import { moduleTools, defineConfig } from '@modern-js/module-tools';
-
+import sass from "sass"
 export default defineConfig({
   buildConfig: {
+    style: {
+      sass: {
+        implementation: sass,
+      },
+      inject: true,
+    },
+    minify: {
+      compress: {
+        drop_console: true,
+      },
+    },
     // 避免打包 React
     externals: ['react', 'react-dom'],
   },
